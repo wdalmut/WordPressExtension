3284b316002051f8d6f644da6cf21feb65b79d14
Walter Dal Mut <walter.dalmut@gmail.com>
2014-03-16 10:51:19 +0100

    Before Scenario hook load wp config files

--- features/login.feature
+++ features/login.feature
@@ -1,7 +1,7 @@
 Feature: Login
     In order to login into my WP website
     As an user
-    I need to know my credential
+    I need to know my credentials
 
     Background:
         Given I have a vanilla wordpress installation
@@ -12,4 +12,22 @@ Feature: Login
         And I fill in "pwd" with "test"
         And I press "Log In"
         Then I should be on "/wp-admin/"
+
+    Scenario: An existing user tries to login with a wrong password
+        When I am on "/wp-login.php"
+        And I fill in "user_login" with "admin"
+        And I fill in "pwd" with "password"
+        And I press "Log In"
+        Then I should be on "/wp-login.php"
+        And I should see "ERROR: The password you entered for the username admin is incorrect."
+
+    Scenario: An invalid user tries to login with a not existing user
+        When I am on "/wp-login.php"
+        And I fill in "user_login" with "john"
+        And I fill in "pwd" with "password"
+        And I press "Log In"
+        Then I should be on "/wp-login.php"
+        And I should see "ERROR: Invalid username."
+
+
 
--- src/Corley/WordPressContext.php
+++ src/Corley/WordPressContext.php
@@ -10,6 +10,9 @@ use Behat\Gherkin\Node\PyStringNode,
 
 use Behat\MinkExtension\Context\MinkContext;
 
+use Behat\Behat\Event\SuiteEvent,
+    Behat\Behat\Event\ScenarioEvent;
+
 require_once 'PHPUnit/Autoload.php';
 require_once 'PHPUnit/Framework/Assert/Functions.php';
 
@@ -21,44 +24,20 @@ class WordPressContext extends MinkContext
     {
         $this->parameters = $parameters;
 
-        (!defined('WP_INSTALLING')) ? define('WP_INSTALLING', true) : '';
+    }
 
-        (!defined('ABSPATH')) ? define('ABSPATH', $parameters["cms"] . '/') : '';
-        (!defined('WP_CONTENT_DIR')) ? define('WP_CONTENT_DIR', $parameters["cms"] . "/") : '';
-        (!defined('WP_DEBUG')) ? define('WP_DEBUG', true) : '';
-
-        (!defined('DB_NAME')) ? define('DB_NAME', $parameters["db"]["name"]) : '';
-        (!defined('DB_USER')) ? define('DB_USER', $parameters["db"]["user"]) : '';
-        (!defined('DB_PASSWORD')) ? define('DB_PASSWORD', $parameters["db"]["pass"]) : '';
-        (!defined('DB_HOST')) ? define('DB_HOST', $parameters["db"]["host"]) : '';
-        (!defined('DB_CHARSET')) ? define('DB_CHARSET', 'utf8') : '';
-        (!defined('DB_COLLATE')) ? define('DB_COLLATE', '') : '';
-
-        $table_prefix  = 'wp_';   // Only numbers, letters, and underscores please!
-
-        (!defined('WP_TESTS_DOMAIN')) ? define('WP_TESTS_DOMAIN', 'example.org') : '';
-        (!defined('WP_TESTS_EMAIL')) ? define('WP_TESTS_EMAIL', 'admin@example.org') : '';
-        (!defined('WP_TESTS_TITLE')) ? define('WP_TESTS_TITLE', 'Test Blog') : '';
-
-        (!defined('WP_PHP_BINARY')) ? define('WP_PHP_BINARY', 'php') : '';
-
-        (!defined('WPLANG')) ? define('WPLANG', '') : '';
+    /**
+     * @BeforeSuite
+     */
+    public static function useWordPressConfiguration(SuiteEvent $event)
+    {
+        $parameters = $event->getContextParameters();
 
         $_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
-        $_SERVER['HTTP_HOST'] = $parameters["host"];
+        $_SERVER['HTTP_HOST'] = $parameters["wp"]["host"];
         $PHP_SELF = $GLOBALS['PHP_SELF'] = $_SERVER['PHP_SELF'] = '/index.php';
 
-        $mysqli = new \Mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-        $value = $mysqli->multi_query(implode("\n", array(
-            "DROP DATABASE IF EXISTS " . DB_NAME . ";",
-            "CREATE DATABASE " . DB_NAME . ";",
-        )));
-        assertTrue($value);
-
-        ob_start();
-        require_once(ABSPATH . 'wp-settings.php');
-        require_once ABSPATH . 'wp-admin/includes/upgrade.php';
-        ob_get_clean();
+        require_once $parameters["wp"]["cms"] . '/wp-config.php';
     }
 
     /**
@@ -66,6 +45,13 @@ class WordPressContext extends MinkContext
      */
     public function installWordPress()
     {
+        $mysqli = new \Mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        $value = $mysqli->multi_query(implode("\n", array(
+            "DROP DATABASE IF EXISTS " . DB_NAME . ";",
+            "CREATE DATABASE " . DB_NAME . ";",
+        )));
+        assertTrue($value);
+        require_once ABSPATH . 'wp-admin/includes/upgrade.php';
         wp_install( "BDD WP", 'admin', 'walter.dalmut@gmail.com', true, '', 'test' );
     }
 }
